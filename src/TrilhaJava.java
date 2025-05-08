a329ebfdc50912cce1ecb7916eab2e882bd8e7f9
DavidFreita1 <freitasdavid5545@gmail.com>
2025-05-08 02:42:17 -0400

    trilha23

--- src/TrilhaJava.java
+++ src/TrilhaJava.java
@@ -2,5 +2,10 @@ public class TrilhaJava {
     public static void main(String[] args) {
         System.out.print("ola mundo");
 
+        int cep = 2222333;
+        String meuNome = "David Freitas";
+        boolean verdadeiro = false;
+        double fracao = 2.32;
+
     }
 }
